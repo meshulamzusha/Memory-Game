68c1f741113b97bbbc9fa8bac68454007b044f19
Your Name <you@example.com>
2025-10-28 12:12:31 +0200

    add: start_game_message function

---
+++ game/io.py
@@ -0,0 +1,6 @@
+def start_game_message() -> None:
+    """prints a message for game starting"""
+    print("Welcome to the memory game.\n"
+          "Please select the board size.\n"
+          "Type a number and press Enter\n"
+          "The game board will be size x size.")
