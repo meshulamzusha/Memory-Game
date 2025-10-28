3884186d89f77ea2bf7559f2f40532e52b493173
Your Name <you@example.com>
2025-10-28 16:00:04 +0200

    add: create_cards_table function

--- utils/utils.py
+++ utils/utils.py
@@ -4,3 +4,13 @@ def convert_input_type(user_input: str) -> tuple[int, int]:
     """Returns a tuple of coordinates converted from string to int"""
     coordinates = user_input.split(',')
     return int(coordinates[1]), int(coordinates[2])
+
+
+def create_cards_table(size: int) -> list[list]:
+    table = []
+
+    for i in range(size):
+        row = list(range((i * size) + 1, (i + 1) * size))
+        table.append(row)
+
+    return table
