f5969bc0864f283eed0ca4c8f31526009b536700
Your Name <you@example.com>
2025-10-28 15:13:37 +0200

    add: get_cards_index function

--- game/io.py
+++ game/io.py
@@ -17,3 +17,11 @@ def display_table(table: list[list]) -> None:
     """prints a table in a user-friendly way"""
     for row in table:
         print(' '.join(map(str,row)))
+
+
+def get_cards_index() -> tuple[tuple[int, int], tuple[int, int]]:
+    """Returns a tuple of tow tuples for users card flipping"""
+    position_1 = input(f'Enter the first card position that you want to flip in the following format i comma j: ')
+    position_2 = input(f'Enter the other card position that you want to flip in the following format i comma j: ')
+
+    return utils.convert_input_type(position_1), utils.convert_input_type(position_2)
