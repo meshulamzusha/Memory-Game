fffcc118385e2fcf20c2cf4f4e992f2fc2d4f2e4
Your Name <you@example.com>
2025-10-28 17:35:06 +0200

    add: is_flipping function

--- game/game_logic.py
+++ game/game_logic.py
@@ -7,3 +7,14 @@ def is_matching(table: list[list], position_1: tuple[int, int], position_2: tupl
     j2 = position_2[1]
 
     return table[i1][j1] == table[i2][j2]
+
+
+def is_flipped(table: list[list], position_1: tuple[int, int], position_2: tuple[int, int]) -> bool:
+    """Return True if the 2 cards are already matched"""
+    i1 = position_1[0]
+    j1 = position_1[1]
+
+    i2 = position_2[0]
+    j2 = position_2[1]
+
+    return table[i1][j1] != '*' and table[i2][j2] != '*'
