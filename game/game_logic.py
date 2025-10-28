8e9dd5388f66af0b83fbfd4a0f70cc93216b015f
Your Name <you@example.com>
2025-10-28 17:50:48 +0200

    add: update_player_table function

--- game/game_logic.py
+++ game/game_logic.py
@@ -21,4 +21,12 @@ def is_flipped(table: list[list], position_1: tuple[int, int], position_2: tuple
 
 
 def update_player_table(game_table: list[list], display_table: list[list], position_1: tuple[int, int], position_2: tuple[int, int]) -> None:
-    
+    """Update the table thet will be displayed for player"""
+    i1 = position_1[0]
+    j1 = position_1[1]
+
+    i2 = position_2[0]
+    j2 = position_2[1]
+
+    display_table[i1][j1] = game_table[i1][j1]
+    display_table[i2][j2] = game_table[i2][j2]
