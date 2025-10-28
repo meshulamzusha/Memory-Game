a85c9634a0d93eec5baefd0fe25ffc890aa157b4
Your Name <you@example.com>
2025-10-28 15:56:34 +0200

    add: import random

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,5 @@
+import random
+
 def convert_input_type(user_input: str) -> tuple[int, int]:
     """Returns a tuple of coordinates converted from string to int"""
     coordinates = user_input.split(',')
