588acfe5b5468434652753e42b5b1c27c4a12ced
meshulamzusha <nauko763@gmail.com>
2025-10-29 09:30:30 +0200

    fix: correct elements generation in create_cards_table function

--- utils/utils.py
+++ utils/utils.py
@@ -8,7 +8,7 @@ def convert_input_type(user_input: str) -> tuple[int, int]:
 
 def create_cards_table(size: int) -> list[list]:
     """create a list of lists with elements for game cards represents the game table"""
-    elements = [i for i in range(size // 2) for _ in range(2)]
+    elements = [i for i in range(size * 2) for _ in range(2)]
     table = []
 
     for i in range(size):
